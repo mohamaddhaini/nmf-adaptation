a63cd9769929364ec40d4e75addc64b9821a47f3
mohamaddhaini <73993988+mohamaddhaini@users.noreply.github.com>
2022-05-03 17:45:37 +0200

    Update transform.py

--- nmfadapt/dSprites/transform.py
+++ nmfadapt/dSprites/transform.py
@@ -15,7 +15,7 @@ class ResizeImage():
 
 def rr_train(resize_size=(256, 256)):
     return transforms.Compose([
-    transforms.Scale(resize_size),
+    transforms.Resize(resize_size),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
@@ -23,7 +23,7 @@ def rr_train(resize_size=(256, 256)):
 
 def rr_eval(resize_size=(256, 256)):
     return transforms.Compose([
-        transforms.Scale(resize_size),
+        transforms.Resize(resize_size),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
